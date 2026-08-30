a8dd83caf42ffc7b2ad3d32036e8e019d0c64e32
ZakheleKambula <zakhelekambula@gmail.com>
2026-08-29 20:06:10 -0400

    Update comment in foo.js for clarity

--- preparations-chapter-folder/exercise3/preparations_exercises/foo.js
+++ preparations-chapter-folder/exercise3/preparations_exercises/foo.js
@@ -1,4 +1,4 @@
-//Put a file named foo.js in the folder named preparations_exercises and add the code below.
+//Exercise 3: Put a file named foo.js in the folder named preparations_exercises and add the code below.
 let foo = "bar";
 console.log(foo);
 foo;
