d8352a19ab6e233f93b81c75de4cde9cdb2d85df
ZakheleKambula <zakhelekambula@gmail.com>
2026-08-29 20:07:41 -0400

    Fix console log formatting for clarity

--- preparations-chapter-folder/exercise5/identify_constructors.js
+++ preparations-chapter-folder/exercise5/identify_constructors.js
@@ -2,7 +2,7 @@
 console.log("Identify Constructors for each method; classify as either 'Static' or 'instance' method:");
 console.log();
 console.log('  substring - is an instance method of the String constructor.');
-console.log("  create - is a static method of the Object constructor.");
+console.log('  create - is a static method of the Object constructor.');
 console.log('  fromCharCode - is a static method of the String constructor.');
 console.log('  slice - is an instance method of the Array and String constructor.');
 console.log('  toString - is an instance method of the Array, String, Number, Boolean, Date, and Object constructors');
